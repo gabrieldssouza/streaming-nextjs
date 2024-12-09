08bb775fdffae30ae4b7510298b53096dc6a8939
gabrieldssouza <gabrieldssouza@icloud.com>
2024-12-09 19:07:13 -0300

    fix babel bug

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,19 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jest-environment-jsdom",
-  roots: ["<rootDir>"],
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  testEnvironment: 'jest-environment-jsdom',
+  roots: ['<rootDir>'],
   transform: {
-    "^.+\\.(ts|tsx|js|jsx)$": "babel-jest",
+    '^.+\\.(ts|tsx|js|jsx)$': 'ts-jest',
   },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  modulePathIgnorePatterns: ["<rootDir>/node_modules/"],
-  testMatch: ["<rootDir>/src/**/__tests__/**/*.[jt]s?(x)", "<rootDir>/src/**/*.(spec|test).[tj]s?(x)"],
-  transformIgnorePatterns: ["<rootDir>/node_modules/"],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  modulePathIgnorePatterns: ['<rootDir>/node_modules/'],
+  testMatch: ['<rootDir>/src/**/__tests__/**/*.[jt]s?(x)', '<rootDir>/src/**/*.(spec|test).[tj]s?(x)'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
 };
+
+module.exports = createJestConfig(customJestConfig);
