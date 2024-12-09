77060ee7cac3782f3dff69ac3a119b71a6864186
gabrieldssouza <gabrieldssouza@icloud.com>
2024-12-09 19:53:48 -0300

    fix 404 error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function InitialScreen() {
     <div className="absolute inset-0 bg-black bg-opacity-60"></div>
       <div className="relative z-10 flex flex-col items-center">
         <Image src="/saveemovies.png" alt="Logo" width={250} height={100} />
-        <a href="/home">
+        <a href="/Home">
           <button className="w-60 mt-8 h-12 text-black font-bold flex items-center justify-center rounded-lg bg-white hover:bg-gray-200 transition duration-300">
             Entrar
           </button>
